d2a35575d4668c091a646d01a0d3bc4870e4eaeb
luisfe12 <luisfevf12@gmail.com>
2020-09-23 02:35:07 -0500

    La bd

---
+++ README.m
@@ -0,0 +1,15 @@
+Proceso para crear un repositorio:
+git init
+git add -A
+git commit -m "comentario"
+git branch -M master
+gti remote add origin  https://github.com/luisfe12/Ingenieria_Software2.git
+git push -u origin master
+
+
+Prceso para clonar ya agregar al repositorio propio
+
+git clone direccion URL
+git add -A
+git commit -m "comenta"
+git push
